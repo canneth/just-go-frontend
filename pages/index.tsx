024e9239669dd328286c5ed34095eeaf587a551e
canneth <CHJrWithaC@gmail.com>
2021-12-25 19:39:48 +0800

    Fixed buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,15 @@
 import Logo from '@/components/svgs/Logo';
 import Button from '@/components/common/Button';
 import usePageFadeInOut from '@/hooks/usePageFadeInOut';
+import usePageChangeClickHandler from '@/hooks/usePageChangeClickHandler';
 import styles from './index.module.css';
 
 export default function Home() {
 
   const selfRef = usePageFadeInOut();
+
+  const clickHandlerJustGo = usePageChangeClickHandler('/select');
+  const clickHandlerAbout = usePageChangeClickHandler('/about');
 
   return (
     <div ref={selfRef} className={`${styles.overallContainer}`}>
@@ -21,8 +25,8 @@ export default function Home() {
             <h1 className={styles.title}>JustGo!</h1>
           </div>
           <ol className={styles.buttonList}>
-            <li><Button text='JustGo!' pageEl={selfRef.current!} destinationURL='/select' /></li>
-            <li><Button text={`About`} pageEl={selfRef.current!} destinationURL='/about' noBackground /></li>
+            <li><Button text='JustGo!' clickHandler={clickHandlerJustGo} /></li>
+            <li><Button text={`About`} clickHandler={clickHandlerAbout} noBackground /></li>
           </ol>
         </div>
         <Logo extClasses={styles.logoSvg} />
