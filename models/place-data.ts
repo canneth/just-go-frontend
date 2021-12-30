42ec024016b0dde973b403ccdd5fe9c165e9edac
canneth <CHJrWithaC@gmail.com>
2021-12-30 20:04:43 +0800

    Updated model to be accurate to API return structure

--- models/place-data.ts
+++ models/place-data.ts
@@ -1,26 +1,68 @@
 
-// TODO: Adapt this to actual backend.
-// Check the docs: https://developers.google.com/maps/documentation/places/web-service/details
 export interface PlaceData {
-  id: string;
-  // place_id: string;
-  name: string;
-  placeType: string;
-  address: string;
-  openStatus: boolean;
-  openingTime: string;
-  closingTime: string;
-  contactNumber: string;
-  tags: string[];
+  place_id: number;
+  licence: string;
+  osm_type: string;
+  osm_id: number;
+  boundingbox: string[];
+  lat: string;
+  lon: string;
+  display_name: string;
+  class: string;
+  type: string;
+  importance: 0;
+  icon?: string;
+  address?: {
+    shop?: string;
+    amenity?: string;
+    house_number?: string;
+    road?: string;
+    suburb?: string;
+    city?: string;
+    county?: string;
+    postcode?: string;
+    country?: string;
+    country_code?: string;
+  };
+  extratags?: {}
 };
 
-export interface FavouritePlaceCardData {
-  id: string;
-  placeId: string;
-  name: string;
-  placeType: string;
-  tags: string[];
-  photoReference: string;
-};
-
-
+/*
+For example:
+{
+  "place_id": 71344412,
+  "licence": "Data © OpenStreetMap contributors, ODbL 1.0. https://osm.org/copyright",
+  "osm_type": "node",
+  "osm_id": 6774011327,
+  "boundingbox": [
+    "1.2757011",
+    "1.2758011",
+    "103.8472183",
+    "103.8473183"
+  ],
+  "lat": "1.2757511",
+  "lon": "103.8472683",
+  "display_name": "Starbucks, 8, Drop Off - Anson Rd Entrance, Downtown Core, Singapore, Central, 0668811, Singapore",
+  "class": "amenity",
+  "type": "cafe",
+  "importance": 0.11100000000000002,
+  "icon": "https://nominatim.openstreetmap.org/ui/mapicons//food_cafe.p.20.png",
+  "address": {
+    "amenity": "Starbucks",
+    "house_number": "8",
+    "road": "Drop Off - Anson Rd Entrance",
+    "suburb": "Downtown Core",
+    "city": "Singapore",
+    "county": "Central",
+    "postcode": "0668811",
+    "country": "Singapore",
+    "country_code": "sg"
+  },
+  "extratags": {
+    "cuisine": "coffee_shop",
+    "takeaway": "yes",
+    "brand:wikidata": "Q37158",
+    "brand:wikipedia": "en:Starbucks"
+  }
+}
+*/
--- pages/revisit.tsx
+++ pages/revisit.tsx
@@ -7,9 +7,6 @@ import { ACTIVITIES } from '@/globals/constants';
 import { PlaceData } from '@/models/place-data';
 import styles from './revisit.module.css';
 
-/* Dev-only imports */
-import FAKE_FAVOURITES from '@/mocks/fake-favourites';
-
 export default function RevisitPage() {
   const selfRef = usePageFadeInOut();
   const clickHandlerBack = usePageChangeClickHandler('/select');
@@ -18,16 +15,8 @@ export default function RevisitPage() {
 
   function handleChange(e: ChangeEvent<HTMLSelectElement>) {
     const tag = e.currentTarget.value;
-    // TODO: Replace this with actual fetching of data from API.
-    const fetchedFavouritePlaces = FAKE_FAVOURITES.slice(0, 8);
-    setFavouritesList(fetchedFavouritePlaces); // TODO: May want to optimise this.
+    // TODO: Implement actual fetching of data from API.
   }
-
-  useEffect(() => {
-    // TODO: This is only for testing purposes! Remove after you're done!
-    const fetchedFavouritePlaces = FAKE_FAVOURITES.slice(0, 8);
-    setFavouritesList(fetchedFavouritePlaces); // TODO: May want to optimise this.
-  }, []);
 
   return (
     <div ref={selfRef} className={styles.overallContainer}>
