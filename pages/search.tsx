477742ac6ba52b1f51db11391837d4b462bc6bb5
chjrwithac <chjrwithac@gmail.com>
2021-12-31 12:49:18 +0800

    Prototyping the search function

--- pages/search.tsx
+++ pages/search.tsx
@@ -8,13 +8,24 @@ export default function RevisitPage() {
   const selfRef = usePageFadeInOut();
   const clickHandlerBack = usePageChangeClickHandler('/select');
 
+  function clickHandlerSearch(e: MouseEvent) {
+
+  }
+
   return (
     <div ref={selfRef} className={styles.overallContainer}>
       <header className={styles.header}>
-        <a className={styles.returnText} onClick={clickHandlerBack}>I&apos;ve changed my mind, take me back!</a>
+        <a className={styles.returnText} onClick={clickHandlerBack}>
+          I&apos;ve changed my mind, take me back!
+        </a>
         <p className={styles.prefix}>I have a place in mind! I think it was...</p>
         <div className={styles.searchBoxContainer}>
-          <input type='text' className={styles.searchBox} placeholder='...' />
+          <input
+            type='search'
+            className={styles.searchBox}
+            placeholder='...'
+            aria-label="Search for a place"
+          />
           <button className={styles.searchButton}>
             <span
               className='iconify'
