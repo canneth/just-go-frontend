9a5db0a348b293ee6abb85808b66b99d110c8352
canneth <CHJrWithaC@gmail.com>
2022-02-14 18:03:44 +0800

    Somewhat complete

--- src/components/weather-timeline/weather-badge/WeatherBadge.tsx
+++ src/components/weather-timeline/weather-badge/WeatherBadge.tsx
@@ -13,6 +13,7 @@ export default function WeatherBadge(props: WeatherBadgeProps) {
 
   const hour24Format = props.date.getHours();
   const displayTime = hour24Format > 12 ? `${hour24Format - 12} pm` : `${hour24Format} am`
+  const displayText = props.weather.replaceAll(' (Day)', '');
 
   return (
     <div className={`${styles.overallContainer} ${props.current && styles.isCurrent}`}>
