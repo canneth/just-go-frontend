3c65092fd734cd3f7d87958c2723edb3620a3486
canneth <CHJrWithaC@gmail.com>
2022-02-15 22:10:50 +0800

    Reorganising map component structures

---
+++ src/components/map/Map.tsx
@@ -0,0 +1,12 @@
+
+import L from 'leaflet';
+
+interface MapProps {
+  osmId: number;
+}
+
+export default function Map(props: MapProps) {
+  return (
+    <div>I AM A MAP</div>
+  );
+}
--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,26 +1,16 @@
 
 import Head from 'next/head';
-import Script from 'next/script';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
 import usePageFadeInOut from '@/hooks/usePageFadeInOut';
 import styles from './Map.module.css';
+import Map from '@/components/map/Map';
 
-export default function Map() {
+export default function MapPage() {
 
   const selfRef = usePageFadeInOut();
   const router = useRouter();
-  const placeCoords = [router.query.lat, router.query.lon];
-
-  const [L, setL] = useState(null);
-
-  useEffect(() => {
-    console.log('RENDER!');
-    console.log(L);
-    if (!L) return;
-    // TODO: FIX THIS!!
-    var map = L.map('map').setView([51.505, -0.09], 13);
-  }, [L]);
+  console.log(router.query);
+  const osmId = parseFloat(router.query.osmId as string);
 
   return (
     <>
@@ -32,15 +22,8 @@ export default function Map() {
           crossOrigin=''
         />
       </Head>
-      <Script
-        src='https://unpkg.com/leaflet@1.7.1/dist/leaflet.js'
-        integrity='sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=='
-        crossOrigin=''
-        onLoad={() => setL(window.L)}
-      />
       <div ref={selfRef} className={styles.overallContainer}>
-
-        <div id='map' className={styles.map} />
+        <Map osmId={osmId} />
         <div>HELLO</div>
       </div>
     </>
