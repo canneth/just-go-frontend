05dcbe7ff71c70d7ee4b4a6ff58f2cbde2ec5fb9
canneth <CHJrWithaC@gmail.com>
2022-02-21 16:31:30 +0800

    Temporary display implemented

--- src/components/map/weather-card/MapWeatherCard.tsx
+++ src/components/map/weather-card/MapWeatherCard.tsx
@@ -1,14 +1,16 @@
 
+import WeatherTimeline, { TimeSeriesLocalWeather } from '@/components/weather-timeline/WeatherTimeline';
 import styles from './MapWeatherCard.module.css';
 
 interface MapWeatherCardProps {
+  weatherList: TimeSeriesLocalWeather | undefined;
   className?: string;
 }
 
 export default function MapWeatherCard(props: MapWeatherCardProps) {
   return (
     <div className={`${styles.overallContainer} ${props.className}`}>
-      I AM A MAPWEATHERCARD!
+      {props.weatherList && <WeatherTimeline weatherList={props.weatherList} />}
     </div>
   );
 }
