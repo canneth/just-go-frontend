505afcb1a8c812701feb5f71e4855e7bcd0f45be
canneth <CHJrWithaC@gmail.com>
2022-02-22 15:48:51 +0800

    Implemented user global context

---
+++ src/models/User.ts
@@ -0,0 +1,4 @@
+
+export default interface User {
+  // TODO: Flesh this out when backend work has commenced.
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,31 @@
 
 import Head from 'next/head';
 import Script from 'next/script';
-import Layout from '@/components/layout/Layout';
 import type { AppProps } from 'next/app';
+import Layout from '@/components/layout/Layout';
+import User from '@/models/User';
+import PlaceData from '@/models/PlaceData';
 import '@/styles/global.css';
+import { createContext, Dispatch, SetStateAction, useState } from 'react';
+
+const UserContext = createContext<[User | undefined, Dispatch<SetStateAction<User>> | undefined]>([undefined, undefined]);
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+
+  const [user, setUser] = useState<User>();
+  const [userFavorites, setUserFavorites] = useState<Array<PlaceData['osm_id']>>();
+
   return (
     <>
       <Head>
         <title>JustGo!</title>
       </Head>
       <Script src='https://code.iconify.design/2/2.1.0/iconify.min.js' />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <UserContext.Provider value={[user, setUser]}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </UserContext.Provider>
     </>
   );
 }
