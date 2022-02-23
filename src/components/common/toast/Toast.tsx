1c78372185a6a11ff8d0582151e196e73c916385
canneth <CHJrWithaC@gmail.com>
2022-02-23 21:10:40 +0800

    Implemented portal

--- src/components/common/toast/Toast.tsx
+++ src/components/common/toast/Toast.tsx
@@ -1,4 +1,6 @@
 
+import { useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
 import styles from './Toast.module.css';
 
 interface ToastProps {
@@ -7,11 +9,21 @@ interface ToastProps {
 }
 
 export default function Toast(props: ToastProps) {
-  return (
+
+  const [mounted, setMounted] = useState<boolean>();
+
+  useEffect(() => {
+    setMounted(true);
+    return () => setMounted(false);
+  }, []);
+
+  const toastJsx = (
     <div className={styles.overallContainer}>
       <div className={`${styles.toast} ${props.show ? styles.show : null}`}>
         {props.text}
       </div>
     </div>
   );
+
+  return mounted ? createPortal(toastJsx, document.getElementById('main')!) : null;
 }
