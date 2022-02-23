fb758d0d2de9e9ccc675d053cafe24c8c65334b1
canneth <CHJrWithaC@gmail.com>
2022-02-23 15:05:43 +0800

    Optimised with React.memo

--- src/components/weather-timeline/WeatherTimeline.tsx
+++ src/components/weather-timeline/WeatherTimeline.tsx
@@ -1,4 +1,5 @@
 
+import { memo } from 'react';
 import { Forecast } from '@/models/WeatherForecast';
 import WeatherBadge from './weather-badge/WeatherBadge';
 import styles from './WeatherTimeline.module.css';
@@ -10,12 +11,24 @@ interface WeatherTimelineProps {
   className?: string;
 }
 
-export default function WeatherTimeline(props: WeatherTimelineProps) {
+function compareWeatherTimelineProps(prevProps: WeatherTimelineProps, nextProps: WeatherTimelineProps) {
+  return (
+    prevProps.weatherList.length === nextProps.weatherList.length
+    && prevProps.weatherList.every((prevWeather, i) => prevWeather.weather === nextProps.weatherList[i].weather)
+  );
+}
+
+const WeatherTimeline = memo((props: WeatherTimelineProps) => {
   return (
     <div className={`${styles.overallContainer} ${props.className}`}>
       {
-        props.weatherList.map(x => <WeatherBadge key={x.date.toISOString()} date={x.date} weather={x.weather} current={x.current} />)
+        props.weatherList.map(x => (
+          <WeatherBadge key={x.date.toISOString()} date={x.date} weather={x.weather} current={x.current} />
+        ))
       }
     </div>
   );
-}
+}, compareWeatherTimelineProps);
+WeatherTimeline.displayName = 'WeatherTimeline';
+
+export default WeatherTimeline;
