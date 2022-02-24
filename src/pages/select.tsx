511d88312637419b1a0a0301d0f7cd31ec498904
canneth <CHJrWithaC@gmail.com>
2022-02-24 17:17:09 +0800

    Added toasts for missing features

--- src/pages/select.tsx
+++ src/pages/select.tsx
@@ -1,4 +1,6 @@
 
+import { MouseEvent, useRef, useState } from 'react';
+import Toast from '@/components/common/toast-manager/toast/Toast';
 import usePageChangeClickHandler from '@/hooks/usePageChangeClickHandler';
 import usePageFadeInOut from '@/hooks/usePageFadeInOut';
 import styles from './select.module.css';
@@ -10,16 +12,31 @@ export default function SelectPage() {
   const clickHandlerRecommendations = usePageChangeClickHandler('/recommendations');
   const clickHandlerSearch = usePageChangeClickHandler('/search');
 
+  const [showToast, setShowToast] = useState<boolean>(false);
+  const toastTimeoutRef = useRef<NodeJS.Timeout>();
+  const toastDuration = 2000;
+
+  function clickHandlerWithNotImplementedToast(_: MouseEvent) {
+    if (showToast) return;
+    const existingTimeout = toastTimeoutRef.current;
+    if (existingTimeout) clearTimeout(existingTimeout);
+    setShowToast(true);
+    toastTimeoutRef.current = setTimeout(() => setShowToast(false), toastDuration);
+  }
+
   return (
-    <div ref={selfRef} className={styles.overallContainer}>
-      <h1 className={styles.prompt}>What would you like to do?</h1>
-      <ol className={styles.optionsList}>
-        <li className={styles.option} onClick={clickHandlerRevisit}><a>I want to revisit a favourite spot...</a></li>
-        <p className={styles.divider}>or</p>
-        <li className={styles.option} onClick={clickHandlerRecommendations}><a>I&apos;m looking for a good place...</a></li>
-        <p className={styles.divider}>or</p>
-        <li className={styles.option} onClick={clickHandlerSearch}><a>I have a place in mind!</a></li>
-      </ol>
-    </div>
+    <>
+      <div ref={selfRef} className={styles.overallContainer}>
+        <h1 className={styles.prompt}>What would you like to do?</h1>
+        <ol className={styles.optionsList}>
+          <li className={styles.option} onClick={clickHandlerWithNotImplementedToast}><a>I want to revisit a favourite spot...</a></li>
+          <p className={styles.divider}>or</p>
+          <li className={styles.option} onClick={clickHandlerWithNotImplementedToast}><a>I&apos;m looking for a good place...</a></li>
+          <p className={styles.divider}>or</p>
+          <li className={styles.option} onClick={clickHandlerSearch}><a>I have a place in mind!</a></li>
+        </ol>
+      </div>
+      {showToast && <Toast text='Sorry! This feature has yet to be implemented. ):' duration={toastDuration} />}
+    </>
   );
 }
