3e1b89318e3518948532d3823c86018449458cf8
canneth <CHJrWithaC@gmail.com>
2022-03-07 17:59:46 +0800

    Temporary styling done

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,34 +4,64 @@ import styles from './about.module.css';
 export default function About() {
   return (
     <div className={styles.overallContainer}>
-      <h1>Attributions</h1>
-      <h2>Weather Data</h2>
-      <p>
-        All weather data is pulled from the Weather Forecast dataset&apos;s&nbsp;
-        <a href='https://data.gov.sg/dataset/WeatherForecast?view_id=9bfd2345-62ed-491d-ba2c-940b3eaee39c&resource_id=571ef5fb-ed31-48b2-85c9-61677de42ca9' rel='noreferrer'>
-          2-hour Weather Forecast API&nbsp;
-        </a>
-        which is made available under the terms of the&nbsp;
-        <a href='https://data.gov.sg/open-data-licence' rel='noreferrer'>
-          Singapore Open Data Licence version 1.0
-        </a>.
-      </p>
-      <h2>Place Data</h2>
-      <p>
-        Place search and place data is powered by the&nbsp;
-        <a href='https://nominatim.org/release-docs/develop/api/Overview/' rel='noreferrer'>
-          Nominatim API
-        </a>,
-        and the maps and map tiles used are provided by&nbsp;
-        <a href='https://www.openstreetmap.org/' rel='noreferrer'>
-          OpenStreetMap (OSM)
-        </a>.
-        <br />
-        Both the Nominatim API and OSM maps draw on the OpenStreetMap (OSM) data made available under the terms of the&nbsp;
-        <a href='https://www.openstreetmap.org/copyright' rel='noreferrer'>
-          Open Database License
-        </a>.
-      </p>
-    </div>
+      <section className={styles.section}>
+        <h1 className={styles.sectionTitle}>{`What's this all about?`}</h1>
+        <div className={styles.sectionContent}>
+          <section className={styles.subSection}>
+            <div className={styles.subSectionContent}>
+              <p className={styles.subSectionParagraph}>
+                {`Feel the sudden urge to be out and about, but just can't think of the
+                place to go? This humble app helps you decide on that special somewhere!`}
+              </p>
+              <p className={styles.subSectionParagraph}>
+                This project is still under development. You may follow its progress&nbsp;
+                <a href='https://github.com/canneth/just-go-frontend#readme' rel='noreferrer' target='_blank'>
+                  here
+                </a>.
+              </p>
+            </div>
+          </section>
+        </div>
+      </section>
+      <section className={styles.section}>
+        <h1 className={styles.sectionTitle}>Attributions</h1>
+        <div className={styles.sectionContent}>
+          <section className={styles.subSection}>
+            <h2 className={styles.subSectionTitle}>Weather Data</h2>
+            <div className={styles.subSectionContent}>
+              <p className={styles.subSectionParagraph}>
+                All weather data is pulled from the&nbsp;
+                <a href='https://data.gov.sg/dataset/weather-forecast?resource_id=571ef5fb-ed31-48b2-85c9-61677de42ca9' rel='noreferrer' target='_blank'>
+                  2-hour Weather Forecast API&nbsp;
+                </a>
+                made available under the&nbsp;
+                <a href='https://data.gov.sg/open-data-licence' rel='noreferrer' target='_blank'>
+                  Singapore Open Data Licence version 1.0
+                </a>.
+              </p>
+            </div>
+          </section>
+          <section className={styles.subSection}>
+            <h2 className={styles.subSectionTitle}>Place Data</h2>
+            <div className={styles.subSectionContent}>
+              <p className={styles.subSectionParagraph}>
+                Place data and search is powered by the&nbsp;
+                <a href='https://nominatim.org/release-docs/develop/api/Overview/' rel='noreferrer' target='_blank'>
+                  Nominatim API
+                </a>.
+                Maps and map tiles used are provided by&nbsp;
+                <a href='https://www.openstreetmap.org/' rel='noreferrer' target='_blank'>
+                  OpenStreetMap (OSM)
+                </a>.
+                Both draw on the OSM data made available under the&nbsp;
+                <a href='https://www.openstreetmap.org/copyright' rel='noreferrer' target='_blank'>
+                  Open Database License
+                </a>.
+              </p>
+            </div>
+          </section>
+        </div>
+      </section >
+    </div >
   )
 }
