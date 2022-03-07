21dfc1667f546a7a755e2f6fa9d5feb4131dbc28
canneth <CHJrWithaC@gmail.com>
2022-03-07 16:57:59 +0800

    Refactored to use new weatherStore

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -2,32 +2,30 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import dynamic from 'next/dynamic';
-import { useContext, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import MapPlaceCard from '@/components/map/place-card/MapPlaceCard';
 import MapWeatherCard from '@/components/map/weather-card/MapWeatherCard';
-import { TimeSeriesLocalWeather } from '@/components/weather-timeline/WeatherTimeline';
 import Button from '@/components/common/button/Button';
 import PlaceData from '@/models/PlaceData';
-import ForecastAPIResponse from '@/models/WeatherForecast';
 import usePageFadeInOut from '@/hooks/usePageFadeInOut';
 import usePageChangeClickHandler from '@/hooks/usePageChangeClickHandler';
-import haversineDistance, { LatLonCoords } from '@/utils/haversineDistance';
+import { weatherStore } from './_app';
 import styles from './map.module.css';
+import { observer } from 'mobx-react';
 
 const Map = dynamic(() => import('@/components/map/Map'), { ssr: false });
 
-export default function MapPage() {
+export default observer(function MapPage() {
 
   const router = useRouter();
   const osmIdWithType = router.query.osmIdWithType as string;
 
   const selfRef = usePageFadeInOut();
   const [placeData, setPlaceData] = useState<PlaceData>();
-  const [weatherList, setWeatherList] = useState<TimeSeriesLocalWeather>();
-  const [isFavorited, setIsFavorited] = useState<boolean>();
 
   const placePostalCode = placeData?.address?.postcode;
+  const placeLatLon = placeData ? [parseFloat(placeData.lat), parseFloat(placeData.lon)] : undefined;
 
   const clickHandlerBack = usePageChangeClickHandler('/search');
   const clickHandlerGoogleMaps = usePageChangeClickHandler(`https://www.google.com/maps/place/Singapore+${placePostalCode}`, { toExternal: true });
@@ -45,68 +43,12 @@ export default function MapPage() {
       )).data[0];
       if (!placeData) return;
       setPlaceData(placeData);
-      // Grab and format current date-times for querying weather API.
-      function dateToQueryString(date: Date) {
-        return encodeURIComponent(`
-          ${date.getFullYear()}
-          -${`${date.getMonth() + 1}`.padStart(2, '0')}
-          -${`${date.getDate()}`.padStart(2, '0')}
-          T${`${date.getHours()}`.padStart(2, '0')}
-          :${`${date.getMinutes()}`.padStart(2, '0')}
-          :00
-        `.replaceAll(/\s/g, ''));
-      }
-      const now = new Date;
-      const nowDateQueryString = dateToQueryString(now);
-      now.setHours(now.getHours() - 2);
-      const pastDateQueryString = dateToQueryString(now);
-      now.setHours(now.getHours() - 2);
-      const furtherPastDateQueryString = dateToQueryString(now);
-      // Make the API call to fetch weather forecast for 2 hours from now.
-      const nextWeather = (await axios.get<ForecastAPIResponse>(
-        `https://api.data.gov.sg/v1/environment/2-hour-weather-forecast?
-          date_time=${nowDateQueryString}
-        `.replaceAll(/\s/g, '')
-      )).data;
-      // Make the API call to fetch current weather.
-      const currWeather = (await axios.get<ForecastAPIResponse>(
-        `https://api.data.gov.sg/v1/environment/2-hour-weather-forecast?
-          date_time=${pastDateQueryString}
-        `.replaceAll(/\s/g, '')
-      )).data;
-      // Make the API call to fetch weather for 2 hours ago.
-      const pastWeather = (await axios.get<ForecastAPIResponse>(
-        `https://api.data.gov.sg/v1/environment/2-hour-weather-forecast?
-          date_time=${furtherPastDateQueryString}
-        `.replaceAll(/\s/g, '')
-      )).data;
-      // Find closest weather station to lookup weather data from.
-      const placeCoords: LatLonCoords = [parseFloat(placeData.lat), parseFloat(placeData.lon)];
-      let shortestDistance = Infinity;
-      const nearestWeatherStation = nextWeather?.area_metadata.reduce((agg, station) => {
-        const stationCoords: LatLonCoords = [station.label_location.latitude, station.label_location.longitude];
-        const currDistance = haversineDistance(stationCoords, placeCoords);
-        if (currDistance >= shortestDistance) return agg;
-        shortestDistance = currDistance;
-        return station;
-      });
-      // Build local time-series weather for PlaceCard.
-      const pastWeatherHere = pastWeather?.items[0].forecasts.find(x => x.area === nearestWeatherStation?.name)?.forecast;
-      const currWeatherHere = currWeather?.items[0].forecasts.find(x => x.area === nearestWeatherStation?.name)?.forecast;
-      const nextWeatherHere = nextWeather?.items[0].forecasts.find(x => x.area === nearestWeatherStation?.name)?.forecast;
-      const nowDate = new Date;
-      const pastDate = new Date;
-      const nextDate = new Date;
-      pastDate.setHours(nowDate.getHours() - 2);
-      nextDate.setHours(nowDate.getHours() + 2);
-      const weatherList: TimeSeriesLocalWeather = [
-        { date: pastDate, weather: pastWeatherHere, current: false },
-        { date: nowDate, weather: currWeatherHere, current: true },
-        { date: nextDate, weather: nextWeatherHere, current: false }
-      ];
-      setWeatherList(weatherList);
     })();
   }, [osmIdWithType]);
+
+  useEffect(() => {
+    weatherStore.updateWeatherData();
+  }, []);
 
   return (
     <>
@@ -125,7 +67,13 @@ export default function MapPage() {
         <div className={styles.foregroundContainer}>
           <div className={styles.sidePanelsContainer}>
             {placeData && <MapPlaceCard className={styles.placeCard} placeData={placeData} tagList={['work', 'dine']} />}
-            {placeData && <MapWeatherCard className={styles.weatherCard} weatherList={weatherList} />}
+            {
+              placeData && placeLatLon &&
+              <MapWeatherCard
+                className={styles.weatherCard}
+                weatherList={weatherStore.getWeatherTimeSeriesNearLatLon(placeLatLon[0], placeLatLon[1])}
+              />
+            }
           </div>
           <div className={styles.bottomBarContainer}>
             <Button text='Back' clickHandler={clickHandlerBack} />
@@ -135,4 +83,4 @@ export default function MapPage() {
       </div>
     </>
   );
-}
+});
