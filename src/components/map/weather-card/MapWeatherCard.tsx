8bfddaa8c643d833c9c6cc80b6a027fb3dcdfd64
canneth <CHJrWithaC@gmail.com>
2022-03-07 16:58:17 +0800

    Refactored to cater to new weatherStore

--- src/components/map/weather-card/MapWeatherCard.tsx
+++ src/components/map/weather-card/MapWeatherCard.tsx
@@ -1,16 +1,17 @@
 
-import WeatherTimeline, { TimeSeriesLocalWeather } from '@/components/weather-timeline/WeatherTimeline';
+import WeatherTimeline from '@/components/weather-timeline/WeatherTimeline';
+import WeatherTimeSeries from '@/models/WeatherTimeSeries';
 import styles from './MapWeatherCard.module.css';
 
 interface MapWeatherCardProps {
-  weatherList: TimeSeriesLocalWeather | undefined;
+  weatherList: WeatherTimeSeries;
   className?: string;
 }
 
 export default function MapWeatherCard(props: MapWeatherCardProps) {
   return (
     <div className={`${styles.overallContainer} ${props.className}`}>
-      {props.weatherList && <WeatherTimeline className={styles.weatherTimeline} weatherList={props.weatherList} />}
+      <WeatherTimeline className={styles.weatherTimeline} weatherList={props.weatherList} />
     </div>
   );
 }
