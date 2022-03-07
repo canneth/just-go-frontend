dc8a6ea150d65b173a0a26856c2ea40f2e8d3379
canneth <CHJrWithaC@gmail.com>
2022-03-07 16:41:37 +0800

    Changed to experiment with weatherStore

--- src/pages/sandbox.tsx
+++ src/pages/sandbox.tsx
@@ -1,17 +1,24 @@
 
+import { MouseEvent } from 'react';
 import Button from '@/components/common/button/Button';
-import styles from './sandbox.module.css';
 import usePageFadeInOut from '@/hooks/usePageFadeInOut';
-import { toast } from '@/components/common/toast-manager/ToastManager';
+import WeatherStore from '@/stores/domain-stores/WeatherStore';
+import styles from './sandbox.module.css';
+
+const weatherStore = new WeatherStore;
 
 export default function Sandbox() {
 
   const selfRef = usePageFadeInOut();
 
+  function clickHandler(e: MouseEvent) {
+    weatherStore.updateWeatherData();
+  }
+
   return (
     <>
       <div ref={selfRef} className={styles.overallContainer}>
-        <Button text={`Trigger toast`} clickHandler={() => toast('Hello am toast!')} />
+        <Button text={`Do stuff`} clickHandler={clickHandler} />
       </div>
     </>
   );
