3f86e0808177ad2b913ec9dfd61c29b9e85d2357
canneth <CHJrWithaC@gmail.com>
2022-03-13 15:18:10 +0800

    Added weatherApiHandlers

--- src/api-mocks/mockApiServer.ts
+++ src/api-mocks/mockApiServer.ts
@@ -1,7 +1,8 @@
 
 import { setupServer } from 'msw/node';
 import placeApiHandlers from './handlers/placeApiHandlers';
+import weatherApiHandlers from './handlers/weatherApiHandlers';
 
-const mockApiServer = setupServer(...placeApiHandlers);
+const mockApiServer = setupServer(...placeApiHandlers, ...weatherApiHandlers);
 
 export default mockApiServer;
--- src/api-mocks/mockApiWorker.ts
+++ src/api-mocks/mockApiWorker.ts
@@ -1,7 +1,8 @@
 
 import { setupWorker } from 'msw';
 import placeApiHandlers from './handlers/placeApiHandlers';
+import weatherApiHandlers from './handlers/weatherApiHandlers';
 
-const mockApiWorker = setupWorker(...placeApiHandlers);
+const mockApiWorker = setupWorker(...placeApiHandlers, ...weatherApiHandlers);
 
 export default mockApiWorker;
