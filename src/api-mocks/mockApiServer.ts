dd79801b0ce2499c51c4ca875f38dc259cf7a90b
canneth <CHJrWithaC@gmail.com>
2022-03-13 15:31:31 +0800

    Re-commit to fix deployment bug

--- src/api-mocks/mockApiServer.ts
+++ src/api-mocks/mockApiServer.ts
@@ -1,8 +1,8 @@
 
-import { setupServer } from 'msw/node';
-import placeAPIHandlers from './handlers/placeAPIHandlers';
-import weatherAPIHandlers from './handlers/weatherAPIHandlers';
+// import { setupServer } from 'msw/node';
+// import placeAPIHandlers from './handlers/placeAPIHandlers';
+// import weatherAPIHandlers from './handlers/weatherAPIHandlers';
 
-const mockAPIServer = setupServer(...placeAPIHandlers, ...weatherAPIHandlers);
+// const mockAPIServer = setupServer(...placeAPIHandlers, ...weatherAPIHandlers);
 
-export default mockAPIServer;
+// export default mockAPIServer;
