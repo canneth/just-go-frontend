b766188d508aca243fd36106870f0211e909ccbb
canneth <CHJrWithaC@gmail.com>
2022-03-15 18:07:25 +0800

    Fleshed out the rest of the test

--- src/stores/domain-stores/WeatherStore.test.ts
+++ src/stores/domain-stores/WeatherStore.test.ts
@@ -1,6 +1,8 @@
 
-import WeatherTimeSeries from '@/models/WeatherTimeSeries';
-import WeatherStore, { LatLon } from './WeatherStore';
+import { placeholderCurrWeather, placeholderNextWeather, placeholderPrevWeather } from '@/api-mocks/handlers/weatherAPIHandlers';
+import mockAPIServer from '@/api-mocks/mockAPIServer';
+import { rest } from 'msw';
+import WeatherStore, { parseAPIResponse } from './WeatherStore';
 
 describe('WeatherStore', () => {
   beforeEach(() => {
@@ -9,7 +11,6 @@ describe('WeatherStore', () => {
   });
   describe('each instance', () => {
     it('has weatherData initialised as undefined', () => {
-      jest.spyOn(WeatherStore.prototype, 'updateWeatherData').mockImplementation();
       const weatherStore = new WeatherStore;
       expect(weatherStore.weatherData).toBeUndefined();
     });
@@ -26,89 +27,111 @@ describe('WeatherStore', () => {
       expect(WeatherStore.dateToQueryString(dateTime)).toBe(expectedResult);
     });
   });
-  describe('hasLatLon(lat: number, lon: number)', () => {
-    describe('if weatherData is undefined', () => {
-      it('returns undefined', () => {
-        jest.spyOn(WeatherStore.prototype, 'updateWeatherData').mockImplementation();
-        const weatherStore = new WeatherStore;
-        expect(weatherStore.weatherData).toBeUndefined();
-        expect(weatherStore.hasLatLon(12, 23)).toBeUndefined();
-      });
-    });
-    describe('if weatherData is defined', () => {
-      it('returns false if [lat, lon] does not exist in weatherData', async () => {
-        const weatherStore = new WeatherStore;
-      });
-      it('returns true if plat, lon] exists in weatherData', () => {
-
-      });
-    });
-  });
   describe('getNearestStationLatLon(lat: number, lon: number)', () => {
     describe('if weatherData is undefined', () => {
       it('returns undefined', () => {
-
+        const weatherStore = new WeatherStore;
+        expect(weatherStore.getNearestStationLatLon(0, 0)).toBeUndefined();
       });
     });
     describe('if weatherData is defined', () => {
-      it('if [lat, lon] exists in weatherData, returns exactly that', () => {
-
+      it('if [lat, lon] exists in weatherData, returns exactly that', async () => {
+        const weatherStore = new WeatherStore;
+        await weatherStore.updateWeatherData();
+        const includedLatLon = Array.from(weatherStore.weatherData!.keys())[0];
+        expect(weatherStore.getNearestStationLatLon(...includedLatLon)).toStrictEqual(includedLatLon);
       });
-      it('if [lat, lon] does not exist in weatherData, calculates and returns the closest', () => {
-
+      it('if [lat, lon] does not exist in weatherData, calculates and returns the closest', async () => {
+        const weatherStore = new WeatherStore;
+        await weatherStore.updateWeatherData();
+        const nearestLatLon = Array.from(weatherStore.weatherData!.keys())[0];
+        const latLon = [nearestLatLon[0] + 0.001, nearestLatLon[1]] as const;
+        expect(weatherStore.getNearestStationLatLon(...latLon)).toStrictEqual(nearestLatLon);
       });
     });
   });
   describe('getWeatherTimeSeriesNearLatLon(lat: number, lon: number)', () => {
     describe('if weatherData is undefined', () => {
       it('returns undefined', () => {
-
+        const weatherStore = new WeatherStore;
+        expect(weatherStore.getWeatherTimeSeriesNearLatLon(0, 0)).toBeUndefined();
       });
     });
     describe('if weatherData is defined', () => {
-      it('if [lat, lon] exists in weatherData, returns the WeatherTimeSeries there', () => {
-
+      it('if [lat, lon] exists in weatherData, returns the WeatherTimeSeries there', async () => {
+        const weatherStore = new WeatherStore;
+        await weatherStore.updateWeatherData();
+        const [includedLatLon, includedWeatherTimeSeries] = Array.from(weatherStore.weatherData!.entries())[0];
+        expect(weatherStore.getWeatherTimeSeriesNearLatLon(...includedLatLon)).toStrictEqual(includedWeatherTimeSeries);
       });
-      it('if [lat, lon] does not exist in weatherData, returns the WeatherTimeSeries closest to it', () => {
-
+      it('if [lat, lon] does not exist in weatherData, returns the WeatherTimeSeries closest to it', async () => {
+        const weatherStore = new WeatherStore;
+        await weatherStore.updateWeatherData();
+        const [closestLatLon, closestWeatherTimeSeries] = Array.from(weatherStore.weatherData!.entries())[0];
+        const latLon = [closestLatLon[0] + 0.001, closestLatLon[1]] as const;
+        expect(weatherStore.getWeatherTimeSeriesNearLatLon(...latLon)).toStrictEqual(closestWeatherTimeSeries);
       });
     });
   });
   describe('async updateWeatherData()', () => {
-    it('makes GET request(s) to the weather API', () => {
-
+    describe('if all GET request(s) resolve(s) successfully with data', () => {
+      it('weatherData is populated (is no longer undefined)', async () => {
+        const weatherStore = new WeatherStore;
+        await weatherStore.updateWeatherData();
+        expect(weatherStore.weatherData).toBeDefined();
+      });
     });
     describe('if all GET requests are rejected with an error', () => {
-      it('throws no errors', () => {
-
+      it('throws no errors', async () => {
+        mockAPIServer.use(
+          rest.get('https://api.data.gov.sg/v1/environment/2-hour-weather-forecast', (req, res, ctx) => res(ctx.status(400)))
+        );
+        const weatherStore = new WeatherStore;
+        await weatherStore.updateWeatherData();
+        expect(true).toBe(true); // Pass if no error is thrown
       });
-      it('sets weatherData to undefined', () => {
-
-      });
-    });
-    describe('if the GET request resolves successfully with data', () => {
-      it('parses response and populates weatherData', () => {
-
+      it('sets weatherData to undefined', async () => {
+        const weatherStore = new WeatherStore;
+        await weatherStore.updateWeatherData();
+        mockAPIServer.use(
+          rest.get('https://api.data.gov.sg/v1/environment/2-hour-weather-forecast', (req, res, ctx) => res(ctx.status(400)))
+        );
+        await weatherStore.updateWeatherData();
+        expect(weatherStore.weatherData).toBeUndefined();
       });
     });
   });
 });
 
-describe('parseApiResponse(pastWeather: ForecastAPIResponse | undefined, currWeather: ForecastAPIResponse | undefined, nextWeather: ForecastAPIResponse | undefined)', () => {
+describe('parseAPIResponse(pastWeather: ForecastAPIResponse | undefined, currWeather: ForecastAPIResponse | undefined, nextWeather: ForecastAPIResponse | undefined)', () => {
   describe('if all arguments are undefined', () => {
     it('returns undefined', () => {
-
+      expect(parseAPIResponse(undefined, undefined, undefined)).toBeUndefined();
     });
   });
   describe('if at least one argument is defined', () => {
     it('returns a map', () => {
-
+      expect(parseAPIResponse(placeholderPrevWeather, undefined, undefined)).toBeInstanceOf(Map);
+      expect(parseAPIResponse(undefined, placeholderCurrWeather, undefined)).toBeInstanceOf(Map);
+      expect(parseAPIResponse(undefined, undefined, placeholderNextWeather)).toBeInstanceOf(Map);
     });
-    it('in all map entries, the weather is undefined for the date(s) corresponding to the undefined argument(s)', () => {
-
-    });
-    it('in all map entries, the weather is defined for the date(s) corresponding to the defined argument(s)', () => {
-
+    it('in all map entries, the weather is undefined only for the date(s) corresponding to the undefined argument(s)', () => {
+      const map = parseAPIResponse(undefined, placeholderCurrWeather, undefined);
+      const expectedValue = [
+        {
+          date: expect.anything(),
+          weather: undefined
+        },
+        {
+          date: expect.anything(),
+          weather: expect.anything()
+        },
+        {
+          date: expect.anything(),
+          weather: undefined
+        }
+      ];
+      for (const value of map!.values()) expect(value).toStrictEqual(expectedValue);
     });
   });
 });
