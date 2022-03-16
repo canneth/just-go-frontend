62d39ac8c4d429a7dd9539c4246fe59878c64fe2
canneth <CHJrWithaC@gmail.com>
2022-03-16 17:18:37 +0800

    More testing of usePromise

--- src/pages/sandbox.tsx
+++ src/pages/sandbox.tsx
@@ -9,10 +9,10 @@ export default function Sandbox() {
 
   const selfRef = usePageFadeInOut();
 
-  const { isLoading, data, error, run } = usePromise(() => {
+  const { isLoading, resolvedValue, error, run } = usePromise((someArg: String, anotherArg: number) => {
     return new Promise((res, rej) => {
       setTimeout(() => {
-        res('This is the data!');
+        res(`This is the data! someArg: ${someArg} | anotherArg: ${anotherArg}`);
       }, 3000);
     });
   });
@@ -24,10 +24,10 @@ export default function Sandbox() {
   return (
     <>
       <div ref={selfRef} className={styles.overallContainer}>
-        <Button text={`Run async callback!`} clickHandler={run} />
+        <Button text={`Run async callback!`} clickHandler={() => run('someString', 5)} />
         <p>Loading state: {`${isLoading}`}</p>
         <p>Error: {error ? 'ERROR OCCURRED!!' : 'no error'}</p>
-        <p>Data: {data ? JSON.stringify(data) : 'no data'}</p>
+        <p>Data: {resolvedValue ? JSON.stringify(resolvedValue) : 'no data'}</p>
       </div>
     </>
   );
