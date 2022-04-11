27ad12f62bc99eeda3ba0be390cf85c3362c89d4
canneth <CHJrWithaC@gmail.com>
2022-04-12 01:24:51 +0800

    Revamped with MinimizableCard

--- src/components/map/weather-card/MapWeatherCard.tsx
+++ src/components/map/weather-card/MapWeatherCard.tsx
@@ -1,4 +1,5 @@
 
+import MinimizableCard from '@/components/common/minimizable-card/MinimizableCard';
 import WeatherTimeline from '@/components/weather-timeline/WeatherTimeline';
 import WeatherTimeSeries from '@/models/WeatherTimeSeries';
 import styles from './MapWeatherCard.module.css';
@@ -9,9 +10,16 @@ interface MapWeatherCardProps {
 }
 
 export default function MapWeatherCard(props: MapWeatherCardProps) {
+
+  const minimizedContent = (
+    <span className='iconify' data-icon='majesticons:checkbox-list-detail' data-width='100%' data-height='100%' />
+  );
+
   return (
-    <div className={`${styles.overallContainer} ${props.className}`}>
-      <WeatherTimeline className={styles.weatherTimeline} weatherList={props.weatherList} />
-    </div>
+    <MinimizableCard className={`${styles.overallContainer} ${props.className}`} minimizedDiameter={50} minimizedContentJsx={minimizedContent} tooltip='Weather timeline'>
+      <div className={styles.contentContainer}>
+        <WeatherTimeline className={styles.weatherTimeline} weatherList={props.weatherList} />
+      </div>
+    </MinimizableCard>
   );
 }
