dc3947ff8216ada043f9345be541631fea69139c
chjrwithac <chjrwithac@gmail.com>
2022-04-11 16:52:38 +0800

    Revamped to be wrappable in MinimisableCard

--- src/components/map/place-card/MapPlaceCard.tsx
+++ src/components/map/place-card/MapPlaceCard.tsx
@@ -5,6 +5,7 @@ import FavoriteToggle from '@/components/common/favorite-toggle/FavoriteToggle';
 import PlaceData from '@/models/PlaceData';
 import TagLabel from '@/models/TagLabel';
 import styles from './MapPlaceCard.module.css';
+import MinimisableCard from '@/components/common/minimisable-card/MinimisableCard';
 
 interface MapPlaceCardProps {
   placeData: PlaceData;
@@ -13,8 +14,6 @@ interface MapPlaceCardProps {
 }
 
 export default function MapPlaceCard(props: MapPlaceCardProps) {
-
-  const [minimised, setMinimised] = useState<boolean>(false);
 
   // Format place data for display.
   const placeName = props.placeData.display_name.split(', ')[0];
@@ -31,40 +30,31 @@ export default function MapPlaceCard(props: MapPlaceCardProps) {
   `;
 
   return (
-    <div className={`${styles.overallContainer} ${props.className} ${minimised ? styles.minimised : null}`}>
-      {
-        minimised
-          ?
-          <div className={styles.minimisedContentContainer}>
-
+    <MinimisableCard className={props.className} minimisedDiameter={40}>
+      <div className={styles.contentContainer}>
+        <div className={styles.descriptionColumn}>
+          <p className={styles.placeName}>{placeName}</p>
+          <div className={styles.typeLine}>
+            {placeIconSource && <Image src={placeIconSource} alt='Place icon' height={20} width={20} />}
+            <p>{placeType}</p>
           </div>
-          :
-          <div className={styles.expandedContentContainer}>
-            <div className={styles.descriptionColumn}>
-              <p className={styles.placeName}>{placeName}</p>
-              <div className={styles.typeLine}>
-                {placeIconSource && <Image src={placeIconSource} alt='Place icon' height={20} width={20} />}
-                <p>{placeType}</p>
-              </div>
-              <p>{placeAddress}</p>
-              <p>{placeRegion}</p>
-              <ol className={styles.tagList}>
-                {
-                  props.tagList.map(tag => (
-                    <li key={tag} className={styles.tagItem}>
-                      {`${tag.charAt(0).toUpperCase()}${tag.slice(1)}`}
-                    </li>
-                  ))
-                }
-              </ol>
-            </div>
-            <FavoriteToggle
-              className={styles.favoriteToggle}
-              placeId={props.placeData.osm_id}
-            />
-            <button className={styles.minimiseButton} onClick={() => setMinimised(true)} />
-          </div>
-      }
-    </div>
+          <p>{placeAddress}</p>
+          <p>{placeRegion}</p>
+          <ol className={styles.tagList}>
+            {
+              props.tagList.map(tag => (
+                <li key={tag} className={styles.tagItem}>
+                  {`${tag.charAt(0).toUpperCase()}${tag.slice(1)}`}
+                </li>
+              ))
+            }
+          </ol>
+        </div>
+        <FavoriteToggle
+          className={styles.favoriteToggle}
+          placeId={props.placeData.osm_id}
+        />
+      </div>
+    </MinimisableCard>
   );
 }
