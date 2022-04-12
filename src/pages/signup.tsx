45ffb6198c5a9cb82154878a6b486196a3af8080
canneth <CHJrWithaC@gmail.com>
2022-04-12 20:04:19 +0800

    Incomplete skeleton

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,4 +1,5 @@
 
+import Marker from '@/components/svgs/Marker';
 import usePageFadeInOut from '@/hooks/usePageFadeInOut';
 import styles from './signup.module.css';
 
@@ -8,7 +9,35 @@ export default function SignupPage() {
 
   return (
     <div ref={selfRef} className={styles.overallContainer}>
-      HELLO AM SIGNUP PAGE
+      <div className={styles.formContainer}>
+        <div className={styles.headerContainer}>
+          <div className={styles.imageContainer}>
+            <Marker />
+          </div>
+          <h1 className={styles.headerText}>Header</h1>
+        </div>
+        <form className={styles.form}>
+          <ol className={styles.formFieldList}>
+            <li className={styles.emailFieldContainer}>
+              <label htmlFor='email'>Email</label>
+              <input type='text' id='email' name='email' />
+            </li>
+            <li className={styles.passwordFieldContainer}>
+              <label htmlFor='password'>Password</label>
+              <input type='text' id='password' name='password' />
+            </li>
+          </ol>
+        </form>
+      </div>
+      <div className={styles.divider}>or</div>
+      <ol className={styles.fedAuthList}>
+        <li className={styles.googleLoginButtonContainer}>
+
+        </li>
+        <li className={styles.facebookLoginButtonContainer}>
+
+        </li>
+      </ol>
     </div>
   );
 }
