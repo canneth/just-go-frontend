7e3523c0df3c7700be74cbf95caf7159a39c2429
canneth <CHJrWithaC@gmail.com>
2022-04-12 22:50:09 +0800

    Allow for parents to modify styles

--- src/components/common/button/Button.tsx
+++ src/components/common/button/Button.tsx
@@ -7,13 +7,14 @@ interface ButtonProps {
   clickHandler: MouseEventHandler;
   disabled?: boolean;
   noBackground?: boolean;
+  className?: string;
 }
 
 export default function Button(props: ButtonProps) {
   // TODO: Implement CSS styling for disabled version!
   return (
     <button
-      className={`${styles.overallContainer} ${props.noBackground ? styles.noBackground : null}`}
+      className={`${styles.overallContainer} ${props.className} ${props.noBackground ? styles.noBackground : null}`}
       onClick={props.clickHandler}
       disabled={props.disabled}
     >
