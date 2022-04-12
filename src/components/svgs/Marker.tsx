c64bfd653404dced13e45f250569f5237984eea3
canneth <CHJrWithaC@gmail.com>
2022-04-12 23:56:34 +0800

    Revamped for granular control

--- src/components/svgs/Marker.tsx
+++ src/components/svgs/Marker.tsx
@@ -1,7 +1,5 @@
 
 interface MarkerProps {
-  width?: number | string;
-  height?: number | string;
   strokeColor?: string;
   strokeWidth?: number;
   fillColor?: string;
@@ -13,18 +11,39 @@ export default function Marker(props: MarkerProps) {
     <svg
       viewBox='0 0 151 210'
       className={props.className}
-      width={props.width ?? 151}
-      height={props.height ?? 210}
+      width={151}
+      height={210}
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
-      {...props}
     >
       <path
+        id='body'
         fillRule='evenodd'
         clipRule='evenodd'
-        d='M3 74.64C3 34.603 35.46 3 75.5 3 115.541 3 148 34.603 148 74.64c0 55.314-28.937 103.87-72.5 131.36C31.937 178.51 3 129.954 3 74.64ZM52 54.5a6 6 0 1 1 0 12 6 6 0 0 1 0-12Zm48 0a6 6 0 1 1 0 12 6 6 0 0 1 0-12ZM76 129c24.853 0 45-20.147 45-45H31c0 24.853 20.147 45 45 45Z'
+        d='M3 74.6396C3 34.6031 35.4594 3 75.5 3C115.541 3 148 34.6031 148 74.6396C148 129.954 119.063 178.51 75.5 206C31.9369 178.51 3 129.954 3 74.6396Z'
         fill={props.fillColor ?? '#FF5656'}
-        stroke={props.strokeColor ?? '#000'}
+        stroke={props.strokeColor ?? 'black'}
+        strokeWidth={props.strokeWidth ?? 6}
+      />
+      <path
+        id='mouth'
+        d='M76 129C100.853 129 121 108.853 121 84H31C31 108.853 51.1472 129 76 129Z'
+        fill='white'
+        stroke={props.strokeColor ?? 'black'}
+        strokeWidth={props.strokeWidth ?? 6}
+      />
+      <path
+        id='right-eye'
+        d='M100 54.5C103.314 54.5 106 57.1863 106 60.5C106 63.8137 103.314 66.5 100 66.5C96.6863 66.5 94 63.8137 94 60.5C94 57.1863 96.6863 54.5 100 54.5Z'
+        fill='white'
+        stroke={props.strokeColor ?? 'black'}
+        strokeWidth={props.strokeWidth ?? 6}
+      />
+      <path
+        id='left-eye'
+        d='M52 54.5C55.3137 54.5 58 57.1863 58 60.5C58 63.8137 55.3137 66.5 52 66.5C48.6863 66.5 46 63.8137 46 60.5C46 57.1863 48.6863 54.5 52 54.5Z'
+        fill='white'
+        stroke={props.strokeColor ?? 'black'}
         strokeWidth={props.strokeWidth ?? 6}
       />
     </svg >
