b21f29d4638b5af86f0810b64f667b784cee1c8a
canneth <CHJrWithaC@gmail.com>
2022-04-12 22:50:52 +0800

    UI fleshed out

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,43 +1,71 @@
 
+import { MouseEvent } from 'react';
+import Button from '@/components/common/button/Button';
 import Marker from '@/components/svgs/Marker';
+import { toast } from '@/components/common/toast-manager/ToastManager';
 import usePageFadeInOut from '@/hooks/usePageFadeInOut';
+import usePageChangeClickHandler from '@/hooks/usePageChangeClickHandler';
+import { FEATURE_UNAVAILABLE_TEXT } from '@/globals/constants';
 import styles from './signup.module.css';
 
 export default function SignupPage() {
 
   const selfRef = usePageFadeInOut();
+  const clickHandlerLogInRedirect = usePageChangeClickHandler('/login');
+
+  function clickHandlerSignUpButton(e: MouseEvent) {
+    e.preventDefault();
+    toast(FEATURE_UNAVAILABLE_TEXT);
+  }
 
   return (
     <div ref={selfRef} className={styles.overallContainer}>
-      <div className={styles.formContainer}>
+      <div className={styles.dialogueContainer}>
         <div className={styles.headerContainer}>
-          <div className={styles.imageContainer}>
-            <Marker />
-          </div>
-          <h1 className={styles.headerText}>Header</h1>
+          <Marker className={styles.badge} />
+          <h1 className={styles.headerText}>Welcome!</h1>
         </div>
         <form className={styles.form}>
           <ol className={styles.formFieldList}>
-            <li className={styles.emailFieldContainer}>
+            <li className={styles.fieldContainer}>
               <label htmlFor='email'>Email</label>
-              <input type='text' id='email' name='email' />
+              <div className={styles.inputContainer}>
+                <input type='text' id='email' name='email' />
+              </div>
             </li>
-            <li className={styles.passwordFieldContainer}>
+            <li className={styles.fieldContainer}>
               <label htmlFor='password'>Password</label>
-              <input type='text' id='password' name='password' />
+              <div className={styles.inputContainer}>
+                <input type='text' id='password' name='password' />
+              </div>
             </li>
           </ol>
+          <Button className={styles.signUpButton} clickHandler={clickHandlerSignUpButton} text={'Sign up!'} />
         </form>
+        <div className={styles.alreadyHaveAccountContainer}>
+          <p>Already have an account?</p>
+          <a href='' onClick={(e) => { e.preventDefault(); toast(FEATURE_UNAVAILABLE_TEXT); }}>Just log in!</a>
+        </div>
+        <div className={styles.dividerContainer}>
+          <div className={styles.line} />
+          <p>or</p>
+          <div className={styles.line} />
+        </div>
+        <ol className={styles.fedAuthList}>
+          <li className={styles.fedAuthListItem}>
+
+          </li>
+          <li className={styles.fedAuthListItem}>
+
+          </li>
+          <li className={styles.fedAuthListItem}>
+
+          </li>
+          <li className={styles.fedAuthListItem}>
+
+          </li>
+        </ol>
       </div>
-      <div className={styles.divider}>or</div>
-      <ol className={styles.fedAuthList}>
-        <li className={styles.googleLoginButtonContainer}>
-
-        </li>
-        <li className={styles.facebookLoginButtonContainer}>
-
-        </li>
-      </ol>
     </div>
   );
 }
